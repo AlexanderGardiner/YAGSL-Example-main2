45cb0f080b570c9afca3e4ee9ea5b3fa52a0ee22
AlexanderGardiner <misc@alexandergardiner.com>
2024-01-20 13:06:41 -0800

    Pathplanner pids are good for square and for New path

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -81,7 +81,7 @@ public class SwerveSubsystem extends SubsystemBase {
         new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
             new PIDConstants(1, 0.0, 0.0),
             // Translation PID constants
-            new PIDConstants(1.7,
+            new PIDConstants(1.5,
                 0,
                 0),
             // Rotation PID constants
